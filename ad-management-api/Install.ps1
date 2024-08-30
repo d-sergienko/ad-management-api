2d0db89ffbba2e8009db8e17063791085bc71964
Dmitry Sergienko <d-sergienko@users.noreply.github.com>
2024-08-30 13:20:57 +0300

    Add -AllowClobber flag

--- ad-management-api/Install.ps1
+++ ad-management-api/Install.ps1
@@ -1,4 +1,4 @@
-Install-Module -Name UniversalDashboard -Scope CurrentUser
-Install-Module -Name DnsServer -Scope CurrentUser
-Install-Module -Name ActiveDirectory -Force -Scope CurrentUser
-Install-Module -Name PKI -Force -Scope CurrentUser
+Install-Module -Name UniversalDashboard -Scope CurrentUser -AllowClobber
+Install-Module -Name DnsServer -Scope CurrentUser -AllowClobber
+Install-Module -Name ActiveDirectory -Force -Scope CurrentUser -AllowClobber
+Install-Module -Name PKI -Force -Scope CurrentUser -AllowClobber
