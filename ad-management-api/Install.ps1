9399e2c4a43f8f48d76500dc0af2fab7273a17e9
Dmitry Sergienko <d-sergienko@users.noreply.github.com>
2024-08-30 14:37:22 +0300

    UniversalDashboard -> UniversalDashboard.Community

--- ad-management-api/Install.ps1
+++ ad-management-api/Install.ps1
@@ -1,4 +1,4 @@
-$modules = "UniversalDashboard", "DnsServer", "ActiveDirectory", "PKI"
+$modules = "UniversalDashboard.Community", "DnsServer", "ActiveDirectory", "PKI"
 foreach ($m in $modules) {
   if (-not (Get-Module $m -ListAvailable)) {
     Install-Module -Name $m -Scope CurrentUser -AllowClobber -Force
